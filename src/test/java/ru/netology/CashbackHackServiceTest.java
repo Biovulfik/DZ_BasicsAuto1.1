9a2c2ddd9ef2dcbff9c51063f131b6f581f6b31b
Neto test <2001107@mail.ru>
2024-04-01 22:14:31 +0300

    JUnit4 test

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,35 +1,35 @@
 package ru.netology;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void spendingLessThanThousand() {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(900);
         int expected = 100;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void havingSpentMoreThanThousand() {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(1500);
         int expected = 500;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void spendZero() {
         CashbackHackService service = new CashbackHackService();
 
         int actual = service.remain(0);
         int expected = 1000;
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
