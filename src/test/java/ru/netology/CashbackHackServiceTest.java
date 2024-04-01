8860cd275727e5800732c5d3dc77c7c713349d8e
Neto test <2001107@mail.ru>
2024-04-01 19:56:33 +0300

    TestNG

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,32 @@
+package ru.netology;
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+    @org.testng.annotations.Test
+    public void spendingLessThanThousand() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void havingSpentMoreThanThousand() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1500);
+        int expected = 500;
+
+        assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void spendZero() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(0);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+}
