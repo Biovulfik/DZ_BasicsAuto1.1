4b745f09118b0c68aa23f4b518d38f71ea6762d2
Neto test <2001107@mail.ru>
2024-04-05 12:36:39 +0300

    mistake

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -32,4 +32,14 @@ public class CashbackHackServiceTest {
 
         assertEquals(expected, actual);
     }
+
+    @org.junit.Test
+    public void fullAmount() {
+        CashbackHackService service = new CashbackHackService();
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
 }
